835fee7f1e1e45646c7648e03e8af4b58f0c8672
Emily Kanarek <emilyakanarek@gmail.com>
2025-05-05 15:07:53 -0400

    Complete functions calculating distance in blocks and feet

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(street) {
+    if (street >= 42) {
+        return street - 42;
+    } else {
+        return 42 - street;
+    }
+}
+
+function distanceFromHqInFeet(street) {
+    return distanceFromHqInBlocks(street) * 264;
+}
